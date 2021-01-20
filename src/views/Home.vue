01bea4d0379e70e73ded8df147517f8f2013e5b2
Sahil Patel <ssahillppatell@gmail.com>
2021-01-20 23:13:41 +0530

    Test-3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App from Github Actions"/>
+    <HelloWorld msg="Tryin - 3"/>
   </div>
 </template>
 
