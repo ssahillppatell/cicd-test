b7e81fc7eec2c7d07fda89b8aeed375910193faa
Sahil Patel <ssahillppatell@gmail.com>
2021-01-20 23:24:18 +0530

    test-5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Tryin - 3"/>
+    <HelloWorld msg="Finally I think"/>
   </div>
 </template>
 
