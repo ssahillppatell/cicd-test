40aeb859aced3f01ff4e4e0129788e7416beca3b
Sahil Patel <ssahillppatell@gmail.com>
2021-01-20 23:02:12 +0530

    CI/CD from Github Actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App from Travis"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App from Github Actions"/>
   </div>
 </template>
 
